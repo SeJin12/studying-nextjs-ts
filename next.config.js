e10830ecf136f4ac9f835301f4cc5b3c9e94b064
SeJin12 <tpwls8122@naver.com>
2022-08-22 13:31:57 +0900

    config, style...

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = 'ABCD'; // 또는 환경변수 .env file
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/naver',
+        destination:'https://www.naver.com',
+        permanent: false
+      }
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/test/a',
+        destination: `/zyx/${API_KEY}`
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/components/frame/Nav.tsx
+++ pages/components/frame/Nav.tsx
@@ -1,24 +1,43 @@
-import Link from 'next/link';
-import React from 'react';
-import navStyles from "../../../styles/Nav.module.css";
+import Link from "next/link";
+import React from "react";
+// import navStyles from "../../../styles/Nav.module.css";
 
 const Nav = () => {
-    return (
-        <nav className={navStyles.nav}>
-            <ul>
-                <li>
-                    <Link href="/">
-                        <a>Home</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href='/test'>
-                        <a>test</a>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/test">
+            <a>test</a>
+          </Link>
+        </li>
+      </ul>
+      <style jsx>
+        {`
+          nav {
+            padding: 0.5rem 1rem;
+            background: #416583;
+          }
+          ul {
+            display: flex;
+            list-style: none;
+          }
+          li {
+            margin-right: 1rem;
+            color: rgb(255, 255, 255);
+          }
+          a {
+            color: #ffffff;
+          }
+        `}
+      </style>
+    </nav>
+  );
 };
 
 export default Nav;
