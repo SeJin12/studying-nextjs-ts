b2119ebf1ab2ad53e18e4256a9d0eefbabada553
SeJin12 <tpwls8122@naver.com>
2022-08-26 16:43:35 +0900

    axios module, env variable

---
+++ lib/Axios/Common.tsx
@@ -0,0 +1,24 @@
+//https://velog.io/@yiyb0603/React%EC%97%90%EC%84%9C-axios-%EC%BB%A4%EC%8A%A4%ED%85%80%ED%95%98%EA%B8%B0
+
+import axios, { AxiosInstance } from "axios";
+
+const SERVER_URL: string = "http://127.0.0.1:3000/";
+const ACCESS_TOKEN: string | undefined = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+
+export const customAxios: AxiosInstance = axios.create({
+  baseURL: `${SERVER_URL}`,
+  headers: {
+    access_token: `${ACCESS_TOKEN}`,
+  },
+});
+
+export const defaultGet = async (api: string, parameters: any) => {
+    const res = await customAxios.get(api, parameters);
+    return res;
+  };
+  
+
+export const defaultPost = async (api: string, parameters: any) => {
+  const res = await customAxios.post(api, parameters);
+  return res;
+};
--- pages/test.tsx
+++ pages/test.tsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { testAction } from "../store/modules/test";
 
+
+import {defaultPost} from '../lib/Axios/Common';
+
+
 const Test = () => {
   const [text, setText] = useState("");
   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
@@ -11,8 +15,24 @@ const Test = () => {
   const name = useSelector((state: any) => state.test);
 
   useEffect(() => {
-    console.log(name);
+    const ret = defaultPost('/api/hello', undefined).then(res => {
+      console.log('POST response', res);
+    });
+    
+    // (async () => {
+    //   const ret = await 
+    //   const results = await (
+    //     await fetch(
+    //       `/api/hello`
+    //     )
+    //   ).json();
+    //   console.log(results);
+    // })();
+
+    // console.log(name);
   }, [name]);
+
+ 
 
   const dispatch = useDispatch();
   const onDispatch = () => dispatch(testAction(text));
